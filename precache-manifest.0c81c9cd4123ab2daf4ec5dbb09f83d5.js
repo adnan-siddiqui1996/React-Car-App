889132592691fb1169769ade069152323bb0d5fc
Adnan-siddiqui1996 <adnanahmed.siddiqui1996@gmail.com>
2020-10-20 22:56:19 +0500

    Updates

---
+++ precache-manifest.0c81c9cd4123ab2daf4ec5dbb09f83d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40b5aa15a22e9ca0c63ed47497adbf3",
+    "url": "/React-Car-App/index.html"
+  },
+  {
+    "revision": "714bf4be1aa28909d80c",
+    "url": "/React-Car-App/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5e565f6c2918a70f622f",
+    "url": "/React-Car-App/static/css/main.50c93e54.chunk.css"
+  },
+  {
+    "revision": "714bf4be1aa28909d80c",
+    "url": "/React-Car-App/static/js/2.afaa5b80.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/React-Car-App/static/js/2.afaa5b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e565f6c2918a70f622f",
+    "url": "/React-Car-App/static/js/main.52d1d4d1.chunk.js"
+  },
+  {
+    "revision": "269e07257a233999419f",
+    "url": "/React-Car-App/static/js/runtime-main.fb6a923d.js"
+  }
+]);
